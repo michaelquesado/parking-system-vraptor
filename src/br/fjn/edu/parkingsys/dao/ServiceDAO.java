1c5a4b11127af414b326d159ccab16d85068c349
fillipquesado <esquesado@gmail.com>
2014-11-12 09:36:48 -0300

    Criação do metodo getAllService() que retorna um List<Service>.

--- src/br/fjn/edu/parkingsys/dao/ServiceDAO.java
+++ src/br/fjn/edu/parkingsys/dao/ServiceDAO.java
@@ -1,6 +1,10 @@
 package br.fjn.edu.parkingsys.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+
+import org.hibernate.Criteria;
 
 import br.fjn.edu.parkingsys.connection.Connection;
 import br.fjn.edu.parkingsys.model.Service;
@@ -17,6 +21,11 @@ public class ServiceDAO {
 		manager.clear();
 		manager.close();
 
+	}
+	
+	public List<Service> getAllServices(){
+		Criteria criteria = Connection.getSession().createCriteria(Service.class);
+		return criteria.list();
 	}
 
 }
