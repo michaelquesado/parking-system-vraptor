9c41c25370ffd564d8af4cb4ae04f34d38ce485c
Amitai Sousa <amytaisousa@gmail.com>
2014-11-14 11:53:51 -0300

    Adicionando services.

---
+++ src/br/fjn/edu/parkingsys/controller/ServiceController.java
@@ -0,0 +1,16 @@
+package br.fjn.edu.parkingsys.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class ServiceController {
+	
+	@Get("new")
+	public void newService(){
+		
+	}
+	
+	
+
+}
