ac0d1419384f48c67d514632bf798eb49b399634
fillipquesado <esquesado@gmail.com>
2014-11-30 18:24:11 -0300

    criação da classe de conexao e configuração do persistence.xml e criação do banco parkingLog no sgbd.

--- src/br/fjn/edu/parkingsys/dao/log/LogDAO.java
+++ src/br/fjn/edu/parkingsys/dao/log/LogDAO.java
@@ -1,5 +1,29 @@
 package br.fjn.edu.parkingsys.dao.log;
 
+import javax.persistence.EntityManager;
+
+import br.fjn.edu.parkingsys.connection.Connection;
+import br.fjn.edu.parkingsys.model.log.Log;
+
 public class LogDAO {
 	
+	
+	public void register(Log log){
+		
+		EntityManager manager = Connection.getEntityManager();
+
+		manager.getTransaction().begin();
+
+		try {
+			manager.persist(log);
+			manager.getTransaction().commit();
+		} catch (NullPointerException nullPointerException) {
+			manager.getTransaction().rollback();
+		} catch (Exception exception) {
+			manager.getTransaction().rollback();
+		} finally {
+			manager.clear();
+			manager.close();
+		}
+	} 
 }
